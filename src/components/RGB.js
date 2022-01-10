45b238f96bd533f9d2711d084f23921cddadda81
builtbyjosh <pc.refuge@gmail.com>
2022-01-09 22:04:00 -0600

    add color value checks

--- src/components/RGB.js
+++ src/components/RGB.js
@@ -2,14 +2,31 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 const RGB = ({ color, setColor, text }) => {
+  const COLOR_ADJUSTER = 15;
+
+  const handleClick = (change) => {
+    console.log(color, change)
+    if (color + change > 255 || color + change < 0) {
+      return;
+    } else {
+      setColor(color + change);
+    }
+  };
+
   return (
     <View>
       <Text style={styles.subtext}>
         {text}: {color}
       </Text>
-      <Button title={`Increase ${text}`} onPress={() => setColor(color + 1)} />
+      <Button
+        title={`Increase ${text}`}
+        onPress={() => handleClick(COLOR_ADJUSTER)}
+      />
       <Text></Text>
-      <Button title={`Increase ${text}`} onPress={() => setColor(color - 1)} />
+      <Button
+        title={`Decrease ${text}`}
+        onPress={() => handleClick(-1 * COLOR_ADJUSTER)}
+      />
     </View>
   );
 };
